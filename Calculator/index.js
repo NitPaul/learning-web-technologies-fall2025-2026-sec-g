56c39ef221a57c44dd4fe9f0f013339a2faa24ec
NitPaul <aonyendopaul@gmail.com>
2025-11-17 16:30:37 +0600

    Calculator js file created and all function defiend and append to display function implemented

---
+++ Calculator/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.getElementById("display");
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            const value = button.textContent;
+            if (value === "C") {
+                clearDisplay();
+            } 
+            else if (value === "CE") {
+                clearLastDigit();
+            } 
+            else if (value === "=") {
+                calculate();
+            } 
+            else {
+                appendToDisplay(value);
+            }
+
+        });
+
+    });
+
+    
+    function appendToDisplay(input) {
+        display.value += input;
+    }
+
+
+
+});
